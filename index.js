f7c862bf9a966ddcc280cab5fac23179890d6770
Mel Michael <mel.michael007@gmail.com>
2019-02-12 19:15:58 +0100

    Fix route issue

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, withRouter } from 'react-router-dom';
 require('./index.css');
 
 import Header from './src/Header';
@@ -20,6 +20,7 @@ class App extends React.Component {
     }
     this.handleChange = this.handleChange.bind(this);
     this.getWeather = this.getWeather.bind(this);
+    this.getForecast = this.getForecast.bind(this);
   }
 
   handleChange(e) {
@@ -42,17 +43,29 @@ class App extends React.Component {
       });
   }
 
+  getForecast() {
+    console.log(this.state.cityName);
+    const forecast = `${forecastUrl}${this.state.cityName}&type=accurate&APPID=${API_KEY}&cnt=5`;
+    return fetch(forecast)
+      .then(response => response.json())
+      .then(myJson => myJson);
+  }
+
   render() {
+    console.log(this.state)
+    const customProps = {
+      handleChange: this.handleChange,
+      getWeather: this.getWeather,
+      getForecast: this.getForecast,
+      cityName: this.state.cityName
+    };
+
     return (
       <Router>
         <div className="content">
           <Header />
-          <Home 
-            handleChange={this.handleChange} 
-            getWeather={this.getWeather}
-            cityName={this.state.cityName}
-          />
-          <Route path="/forecast" component={Forecast} />
+          <Route exact path="/" render={props => <Home {...props} {...customProps}/>} />
+          <Route path="/forecast" render={props => <Forecast {...props} {...customProps}/>} />
         </div>
       </Router>
     );
@@ -60,8 +73,37 @@ class App extends React.Component {
 }
 
 
-const Forecast = () => (<h3>Forecast Page</h3>);
 
+class Forecast extends React.Component {
+  constructor(props) {
+    super(props);
+    // console.log('fore', props)
+    // this.state = {
+    //   forecast: ''
+    // }
+  }
+  componentDidMount() {
+    this.props.getForecast().then(data => {
+      console.log('data ', data)
+    });
+    // this.setState({forecast: })
+  }
+  render() {
+    // console.log('forecast: ', this.props, this.state)
+    if(this.props.coord) {
+      return (<h3>Forecast Page</h3>);
+    } else {
+      return <Loading />;
+    }
+  }
+}
+
+
+const Loading = () => {
+  return (
+    <h2 style={{textAlign: 'center'}}>Loading...</h2>
+  )
+}
 
 const Home = (props) => (
   <div className="main">
@@ -69,12 +111,10 @@ const Home = (props) => (
     <input type="text" onChange={props.handleChange} value={props.cityName} /> <br />
     <div>
       <button onClick={props.getWeather} style={{ marginRight: 20 }}>Get Current Weather</button>
-      {/* <button onClick={this.getWeather}>Get Forecast</button> */}
       <Link to="/forecast">
-        <button type="button">
-          Get Forecast
-        </button>
+        <button type="button">Get Forecast</button>
       </Link>
+      {/* <button type="button" onClick={() => props.history.push('/forecast')}>Get Forecast</button> */}
     </div>
   </div>
 );
